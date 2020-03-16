7cb78de30cb50a8fb4e6a5286f8c13ef2175d866
rare <reza.rjbi@yahoo.com>
2020-03-16 19:29:39 -0400

    adds train function

--- AmericanSignLanguage_AI/train.h
+++ AmericanSignLanguage_AI/train.h
@@ -12,17 +12,37 @@
 #include <opencv2/opencv.hpp>
 #include "cost.h"
 
-class Cost: public cv::MinProblemSolver::Function {
+const int OPT_ITERATE = 600; /// maximum iteration for optimizer function
+const double OPT_ALPHA = 0.01;  /// the alpha value of the optimizer function
 
-    public:
-        void costFunction(const cv::Mat&,const cv::Mat&,
-                          const cv::Mat&,double,double&,cv::Mat&);
-};
 
-void train() {
-    /// creating a solver that uses conjuction gradient optimization algorithm to find the minimum
-    cv::Ptr<cv::ConjGradSolver> fmincg = cv::ConjGradSolver::create();
-    //cv::Ptr<cv::MinProblemSolver::Function> fun(new Cost());
+void train(cv::Mat& X, cv::Mat& Y, cv::Mat& Theta, cv::Mat& J_history) {
+    J_history = cv::Mat::zeros(OPT_ITERATE, 1, CV_64F);
+    cv::Mat Theta_g = Theta.clone();
+    double J = 0.0;
+    
+    for (int i = 0; i < OPT_ITERATE; i++) {
+        costFunction(Theta, X, Y, lambda, J, Theta_g);
+        Theta -= OPT_ALPHA * Theta_g;
+        J_history.at<double>(i,0) = J;
+    }
 }
 
 #endif /* train_h */
+
+
+
+
+//
+//class Cost: public cv::MinProblemSolver::Function {
+//
+//    public:
+//        void costFunction(const cv::Mat&,const cv::Mat&,
+//                          const cv::Mat&,double,double&,cv::Mat&);
+//};
+//
+//void train() {
+//    /// creating a solver that uses conjuction gradient optimization algorithm to find the minimum
+//    cv::Ptr<cv::ConjGradSolver> fmincg = cv::ConjGradSolver::create();
+//    //cv::Ptr<cv::MinProblemSolver::Function> fun(new Cost());
+//}
